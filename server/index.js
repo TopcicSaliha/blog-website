f631ed60dece2cbf89dc4040ec58755fbe64bf60
saliha <topcicsaliha@gmail.com>
2024-02-12 17:42:35 +0100

    fix: buuild

--- server/index.js
+++ server/index.js
@@ -16,6 +16,14 @@ const router = jsonServer.router("db.json");
 
 const middlewares = jsonServer.defaults();
 
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+  jsonServer.rewriter({
+    "/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
